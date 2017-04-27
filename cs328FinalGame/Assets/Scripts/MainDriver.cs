4b46aa634c2d90ab9c51191d21c19dceabc05655
Matthew Baker <hackmattr@gmail.com>
2017-04-27 16:01:05 -0700

    Initial Commit
    Set up a plane with paddles. Added sample code to show how to make
    paddles appear and disappear

---
+++ cs328FinalGame/Assets/Scripts/MainDriver.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainDriver : MonoBehaviour {
+
+    public GameObject[] Paddle;
+
+	// Use this for initialization
+	void Start () {
+
+        //Paddles are set to false to make them invisible
+        for(int i = 0; i<24; i++)
+        {
+            Paddle[i].SetActive(false);
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Test Code to turn on and off the first paddle with the a key
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Paddle[0].SetActive(true);
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            Paddle[0].SetActive(false);
+        }
+
+	}
+}
