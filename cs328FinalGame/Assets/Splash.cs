fbdecc42bf4cbe7e96a95e942aba53610cf124fa
Matthew Baker <hackmattr@gmail.com>
2017-05-09 20:06:21 -0700

    Splash Screen Added

---
+++ cs328FinalGame/Assets/Splash.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour {
+    public Button StartButton;
+
+    public void ButtonClick()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
